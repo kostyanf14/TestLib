ab778850c26592dc9b54df34c93f1fc1e152349b
kostyanf14 <kostyanf14@live.com>
2018-10-21 19:31:31 +0300

    Set Codelabs company, add it to copyright, add auto increment version

--- TestLib.Worker/Properties/AssemblyInfo.cs
+++ TestLib.Worker/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestLib.Worker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Codelabs")]
 [assembly: AssemblyProduct("TestLib.Worker")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Codelabs ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
