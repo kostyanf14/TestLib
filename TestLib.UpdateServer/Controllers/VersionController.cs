7105e5af9449dcb1999c0660a5f73be8375d8934
Kostiuk Kostiantyn <kostyanf14@live.com>
2019-01-20 15:36:18 +0200

    Added get /api/version method

---
+++ TestLib.UpdateServer/Controllers/VersionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TestLib.UpdateServer.App_Start;
+
+namespace TestLib.UpdateServer.Controllers
+{
+	public class VersionController : ApiController
+	{
+		public string Get()
+		{
+			Version parsedVersion =
+				Version.Parse(File.ReadAllText(Helpers.ReleasesLatestVersionFilePath));
+
+			return parsedVersion.ToString();
+		}
+	}
+}
