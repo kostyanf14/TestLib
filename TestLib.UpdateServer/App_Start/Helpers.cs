25c3e7e903dfaa5fce0003dd25af1acf0b01f426
Kostiuk Kostiantyn <kostyanf14@live.com>
2019-01-20 15:35:14 +0200

    Added creating logs directory

--- TestLib.UpdateServer/App_Start/Helpers.cs
+++ TestLib.UpdateServer/App_Start/Helpers.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 
 namespace TestLib.UpdateServer.App_Start
 {
 	public static class Helpers
 	{
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 		private static string s_tempPath;
 		private static string s_releasesPath;
 
@@ -23,6 +26,10 @@ namespace TestLib.UpdateServer.App_Start
 
 		public static void StartApplication(HttpServerUtility server)
 		{
+			var logPath = server.MapPath("~/Logs");
+			if (!Directory.Exists(logPath))
+				Directory.CreateDirectory(logPath);
+
 			s_tempPath = server.MapPath("~/Temp");
 			if (!Directory.Exists(s_tempPath))
 				Directory.CreateDirectory(s_tempPath);
@@ -30,6 +37,8 @@ namespace TestLib.UpdateServer.App_Start
 			s_releasesPath = server.MapPath("~/App_Data/Releases");
 			if (!Directory.Exists(s_releasesPath))
 				Directory.CreateDirectory(s_releasesPath);
+
+			logger.Info("Application initialization complete");
 		}
 	}
 }
