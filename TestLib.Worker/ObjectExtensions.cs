0beedaf34699a19a2e2be957a8317da1e3b80681
Kostiuk Kostiantyn <kostyanf14@live.com>
2019-01-26 17:16:13 +0200

    Added skipNull paramater for Object.AsJson

--- TestLib.Worker/ObjectExtensions.cs
+++ TestLib.Worker/ObjectExtensions.cs
@@ -1,12 +1,23 @@
-﻿using System.Net.Http;
+﻿using Newtonsoft.Json;
+using System.Net.Http;
 using System.Text;
-using Newtonsoft.Json;
 
 namespace TestLib.Worker
 {
 	internal static class ObjectExtensions
 	{
-		public static StringContent AsJson(this object o)
-		 => new StringContent(JsonConvert.SerializeObject(o), Encoding.UTF8, "application/json");
+		internal static string SerializeObjectToJson(object o, bool skipNull = false)
+		{
+			var settings = new JsonSerializerSettings();
+
+			if (skipNull)
+			{
+				settings.NullValueHandling = NullValueHandling.Ignore;
+			}
+
+			return JsonConvert.SerializeObject(o, Formatting.None, settings);
+		}
+		public static StringContent AsJson(this object o, bool skipNull = false)
+			=> new StringContent(SerializeObjectToJson(o, skipNull), Encoding.UTF8, "application/json");
 	}
 }
