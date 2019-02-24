365bcfb1e75559272b721af3d17efa0813d2e5d6
Kostiuk Kostiantyn <kostyanf14@live.com>
2019-02-24 15:18:46 +0200

    Worker is a service

--- TestLib.WorkerService/Properties/AssemblyInfo.cs
+++ TestLib.WorkerService/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestLib.Worker")]
+[assembly: AssemblyTitle("TestLib.WorkerService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Codelabs")]
-[assembly: AssemblyProduct("TestLib.Worker")]
-[assembly: AssemblyCopyright("Codelabs ©  2018")]
+[assembly: AssemblyProduct("TestLib.WorkerService")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ff84b3e4-fc7b-4b23-83c1-c23554c51314")]
+[assembly: Guid("d58ddeab-b2de-408d-a85c-59ef26c84e50")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
---
+++ TestLib.WorkerService/WorkerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TestLib.Worker;
+
+namespace TestLib.WorkerService
+{
+	public partial class WorkerService : ServiceBase
+	{
+		public WorkerService()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			Application.Get().Start();
+		}
+
+		protected override void OnStop()
+		{
+			Application.Get().End();
+		}
+	}
+}
