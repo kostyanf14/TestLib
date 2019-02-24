c04086469ee70b74fee3dc2ef1cb19e403119c42
Kostiuk Kostiantyn <kostyanf14@live.com>
2019-02-24 19:49:09 +0200

    added command for install service

--- TestLib.WorkerService/Program.cs
+++ TestLib.WorkerService/Program.cs
@@ -1,29 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using NLog;
+using System;
+using System.Configuration.Install;
+using System.Reflection;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using TestLib.Worker;
 
 namespace TestLib.WorkerService
 {
-	static class Program
+	internal static class Program
 	{
+		private static Logger logger;
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
-		static void Main()
+		private static void Main(string[] args)
 		{
-			if (!Application.Get().Init())
-				return;
+			logger = LogManager.GetCurrentClassLogger();
 
-			ServiceBase[] ServicesToRun;
-			ServicesToRun = new ServiceBase[]
+			if (Environment.UserInteractive)
 			{
-				new WorkerService()
-			};
-			ServiceBase.Run(ServicesToRun);
+				logger.Info("Application must run as service, but run at user mode with {0} arguments.", args.Length);
+
+				if (args.Length > 0)
+				{
+					switch (args[0])
+					{
+						case "install":
+							logger.Info("Starting install service");
+
+							ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+							break;
+						case "uninstall":
+							logger.Info("Starting uninstall service");
+
+							ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+							break;
+						default:
+							logger.Info("Unknown command.");
+							break;
+					}
+				}
+			}
+			else
+			{
+				logger.Info("Application run as service");
+
+				if (!Application.Get().Init())
+					return;
+
+				ServiceBase[] ServicesToRun;
+				ServicesToRun = new ServiceBase[]
+				{
+					new WorkerService()
+				};
+				ServiceBase.Run(ServicesToRun);
+			}
 		}
 	}
 }
