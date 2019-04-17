4b8b3f36c51889db58bd7404b7d20e9578dc5c19
Kostiuk Kostiantyn <kostyanf14@live.com>
2019-04-17 19:44:22 +0300

    Added TimeOutHelper

---
+++ TestLib.WorkerService/TimeOutHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestLib.WorkerService
+{
+	internal class TimeOutHelper
+	{
+		private int currentTimeOut = 1;
+
+		public int GetTimeOut()
+			=> currentTimeOut = Math.Min(currentTimeOut * 2, 64);
+
+		public void Zero() => currentTimeOut = 1;
+	}
+}
