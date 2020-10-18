74280145a8660445fa87fe6f17c55bc778487e2d
Kostiantyn Kostiuk <Kostiantyn.Kostiuk@janustech.com>
2020-10-18 16:41:10 +0300

    wip

--- ci.sh
+++ ci.sh
@@ -7,3 +7,10 @@ root_dir="$(cd "$(dirname $(realpath "$0"))" && pwd)"
 cmd //C "${root_dir}/build.cmd"
 
 find "${root_dir}" | grep bin
+
+(
+    cd "${root_dir}/TestLib.WorkerService/bin/x64/Release"
+    zip "${root_dir}/artifacts/WorkerService-x64-Release.zip" -9 -r *
+)
+
+echo -n "value" | openssl dgst -sha256 -hmac "key"
